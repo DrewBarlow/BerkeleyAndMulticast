072b05eb7060c61bd4e8f6372e408601b12842c1
Drew Barlow <abarlow1@umbc.edu>
2022-11-11 17:30:02 -0500

    lksjdhf

--- src/util/berkeley/berkeley.h
+++ src/util/berkeley/berkeley.h
@@ -2,11 +2,11 @@
 #define __BERKELEY_H__
 
 #include <pthread.h>
-#include <semaphore.h>
 #include "../socks/socks.h"
 
 extern pthread_mutex_t clockLock;
 extern pthread_mutex_t threadLock;
+extern int numClocksReceived;
 extern int newClock;
 
 void berkeleySync(int port, int daemonPort, int numMachines, int* logicalClock);
