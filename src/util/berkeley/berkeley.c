87b0bd766a8316de623deb7049a92bfe4d9b70e0
Drew Barlow <abarlow1@umbc.edu>
2022-11-11 16:50:19 -0500

    oh my god

--- src/util/berkeley/berkeley.c
+++ src/util/berkeley/berkeley.c
@@ -1,8 +1,19 @@
 #include "berkeley.h"
 
+pthread_mutex_t clockLock;
+pthread_mutex_t threadLock;
+int newClock;
+
 void berkeleySync(int port, int daemonPort, int numMachines, int* logicalClock) {
   if (isDaemon(port, daemonPort)) {
+    newClock = *logicalClock;
+    pthread_mutex_init(&clockLock, NULL);
+    pthread_mutex_init(&threadLock, NULL)
+
     daemonInit(daemonPort, numMachines, logicalClock);
+
+    pthread_mutex_destroy(&clockLock);
+    pthread_mutex_destroy(&threadLock);
     return;
   }
 
@@ -13,8 +24,45 @@ void berkeleySync(int port, int daemonPort, int numMachines, int* logicalClock) 
 // if the machine is the time daemon, let all other machines connect
 // then interact with them accordingly
 void daemonInit(int daemonPort, int numMachines, int* logicalClock) {
-  // wait until (numMachines - 1) connect to the daemon,
-  // then ask them all for their time and average it
+  // if a process fails to connect then just modify the number of active machines
+  pthread_t threads[numMachines - 1];
+  int sockfd;
+  struct sockaddr_in serverAddr;
+  
+  // create the listening socket
+  if (makeRespSock(daemonPort, numMachines - 1, &sockfd, &serverAddr) != -1) {
+    int currThread = 0;
+
+    pthread_mutex_lock(&threadLock);
+    while (currThread != numMachines - 1) {
+      // receive a connection from a non-daemon machine
+      int connfd = acceptClient(sockfd, &serverAddr);
+
+      if (connfd != -1) {
+        // spawn a new thread to handle the machine
+        int threadRet = pthread_create(&threads[currThread++], NULL, daemonInteraction, (void*) &connfd);
+
+        if (threadRet != 0) {
+          printf("Error spawning a thread for non-daemon machine.\n");
+          break;
+        }
+      }
+    }
+
+    // TODO
+    // wait until all threads have updated newClock
+    newClock /= numMachines;
+
+    // update clock average and resume halted threads
+    pthread_mutex_unlock(&threadLock);
+
+    // join all threads
+    for (int i = 0; i < numMachines - 1; i++) {
+      pthread_join(threads[i], NULL);
+    }
+
+    printf("Daemon finished synchronization. New clock: %d\n", *logicalClock);
+  }
 
   return;
 }
@@ -25,9 +73,76 @@ void daemonInit(int daemonPort, int numMachines, int* logicalClock) {
 // RECV {ALL-CLOCK-VALUES}
 // SEND {NEW-CLOCK-VALUE}
 // RECV "BYE"
-void daemonInteraction() {
+void* daemonInteraction(void* arg) {
+  int connfd = *((int*) arg);
+  char buff[BUFF_SIZE];
+  bzero(buff, BUFF_SIZE);
 
-  return;
+  // read from the socket
+  int totalBytesRead = 0;
+  int ret = read(connfd, buff, BUFF_SIZE);
+  totalBytesRead += ret;
+  if (ret == -1) {
+    perror("Failed to read from buffer.\n");
+    exit(1);
+  }
+
+  // read buffers until we have 1KB of data
+  while (totalBytesRead < BUFF_SIZE) {
+    ret = read(connfd, buff + totalBytesRead, BUFF_SIZE - totalBytesRead);
+    totalBytesRead += ret;
+    if (ret == -1) {
+      perror("Failed to read \"HELLO\" from buffer.\n");
+      exit(1);
+    }
+  }
+
+  bzero(buff, BUFF_SIZE);
+  strncpy(buff, "ACK", 3);
+
+  // send the client an 'ack' to show that we have received the message
+  ret = write(connfd, buff, 3);
+  if (ret == -1) {
+    perror("Failed to \"ACK\" the non-daemon.\n");
+    exit(1);
+  }
+
+  bzero(buff, BUFF_SIZE);
+
+  // read the non-daemon's clock
+  ret = read(connfd, buff, BUFF_SIZE);
+  if (ret == -1) {
+    perror("Failed to read clock from non-daemon.\n");
+    exit(1);
+  }
+
+  // need to do some funky semaphore/mutex stuff here
+  int thisClock = atoi(buff);
+
+  // acquire lock to update global running total
+  pthread_mutex_lock(&clockLock);
+  newClock += thisClock;
+  pthread_mutex_unlock(&clockLock);
+
+  // halt execution of this thread until all clocks have been received
+  // continue execution of this thread with the new average 
+
+  bzero(buff, BUFF_SIZE);
+
+  ret = read(connfd, buff, BUFF_SIZE);
+  if (ret == -1) {
+    perror("Failed to read \"BYE\" from non-daemon.\n");
+    exit(1);
+  }
+
+  if (strncmp(buff, "BYE", 3) == 0) {
+    printf("Non-daemon disconnected.\n");
+  } else {
+    perror("Received something other than \"BYE\" from non-daemon.\n");
+    exit(1);
+  }
+
+  return NULL;
 }
 
 // if this processes' port number is the same as the daemon's,
@@ -58,8 +173,62 @@ void normalInit(int daemonPort, int* logicalClock) {
 // SEND <logicalClock>
 // RECV <newClock>
 // SEND "BYE"
-void normalInteraction(int sockfd, int clifd, int* logicalClock) {
+int normalInteraction(int sockfd, int clifd, int* logicalClock) {
+  char buff[BUFF_SIZE];
+  bzero(buff, BUFF_SIZE);
+  strncpy(buff, "HELLO", 5);
 
-  return;
+  // send an initial HELLO msg
+  int ret = write(sockfd, buff, BUFF_SIZE);
+  if (ret == -1) {
+    perror("Failed to write hello to buffer.\n");
+    return -1;
+  }
+
+  bzero(buff, BUFF_SIZE);
+
+  // read the server's ACK to the client's HELLO
+  ret = read(sockfd, buff, 3);
+  if (ret == -1) {
+    perror("Failed to read ack from buffer.\n");
+    return -1;
+  }
+
+  bzero(buff, BUFF_SIZE);
+
+  // convert the logicalClock of this process to a sendable format
+  itoa(*logicalClock, buff, 10);
+
+  // write the clock value over to the daemon
+  ret = write(sockfd, buff, BUFF_SIZE);
+  if (ret == -1) {
+    perror("Failed to write clock to buffer.\n");
+    return -1;
+  }
+
+  bzero(buff, BUFF_SIZE);
+
+  // receive the new value for the clock
+  ret = read(sockfd, buff, BUFF_SIZE);
+  if (ret == -1) {
+    perror("Failed to read new clock value from buffer.\n");
+    return -1
+  }
+
+  // MAY NEED TO CONVERT IT TO (+/-)n INSTEAD OF RAW VALUE
+  // convert the new clock value to int and reset this clock
+  *logicalClock = atoi(buff);
+
+  bzero(buff, BUFF_SIZE);
+  strncpy(buff, "BYE", 3);
+
+  // send "BYE" to close the connection
+  ret = write(sockfd, buff, 3);
+  if (ret == -1) {
+    perror("Failed to write transaction to buffer.\n");
+    return -1;
+  }
+
+  return 0;
 }
 
--- src/util/berkeley/berkeley.h
+++ src/util/berkeley/berkeley.h
@@ -1,10 +1,16 @@
 #ifndef __BERKELEY_H__
 #define __BERKELEY_H__
 
+#include <pthread.h>
+#include <semaphore.h>
 #include "../socks/socks.h"
 
+extern pthread_mutex_t clockLock;
+extern pthread_mutex_t threadLock;
+extern int newClock;
+
 void berkeleySync(int port, int daemonPort, int numMachines, int* logicalClock);
-void daemonInit(int daemonPort, int numMachines);
+void daemonInit(int daemonPort, int numMachines, int* logicalClock);
 int isDaemon(int port, int daemonPort);
 void normalInit(int daemonPort, int* logicalClock);
 
